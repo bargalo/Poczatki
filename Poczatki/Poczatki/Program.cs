6f377c504c85d9ec79e070feeec9b3ed050fe239
bgipb <bgipb@o2.pl>
2024-01-24 19:34:48 +0100

    analiza danych

--- Poczatki/Poczatki/Program.cs
+++ Poczatki/Poczatki/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Bartek";
+string sex = "male";
+var age = 33;
+if (sex != "male" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");   
+}
