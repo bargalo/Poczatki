0ca0ddce84d6ab9bafd80661d0f153ec4f8a7c39
bgipb <bgipb@o2.pl>
2024-02-04 13:30:06 +0100

    zad dzien10

--- Poczatki/Poczatki/Employee.cs
+++ Poczatki/Poczatki/Employee.cs
@@ -12,7 +12,41 @@
         public string Surname { get; private set; }
         public void AddGrade(float grade)
         {
-            grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("string isnt float");
+            }
+        }
+        public void AddGrade(int grade)
+        {
+            AddGrade((float)grade);
+        }
+        public void AddGrade(long grade)
+        {
+            AddGrade((float)grade);
+        }
+        public void AddGrade(decimal grade)
+        {
+            AddGrade((float)grade);
+        }
+        public void AddGrade(double grade)
+        {
+            AddGrade((float)grade);
         }
         public Statistics GetStatistics()
         {
--- Poczatki/Poczatki/Program.cs
+++ Poczatki/Poczatki/Program.cs
@@ -2,8 +2,9 @@
 
 var employee = new Employee("Bartek", "Galant");
 employee.AddGrade(2);
-employee.AddGrade(2);
-employee.AddGrade(6);
+employee.AddGrade(20000);
+employee.AddGrade("Bartek");
+employee.AddGrade(8);
 var stats = employee.GetStatistics();
 Console.WriteLine($"Average: {stats.Average:N2}");
 Console.WriteLine("Min: " + stats.Min);
